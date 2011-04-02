6d606fc5d9fc0a606e80159bc9fdef9677899c28
Recardo <webmaster@recardo-el-bonito.com>
2011-04-02 11:02:05 +0200

    First Commit

---
+++ config/environment.rb
@@ -0,0 +1,7 @@
+# Load the rails application
+require File.expand_path('../application', __FILE__)
+
+# Initialize the rails application
+Subtools3::Application.initialize!
+
+I18n.default_locale = :de
